7169f87dbc548124eb446306032b4f25be761919
hulul <hulul@VICTUS-BOR3Y>
2026-06-24 20:16:42 +0200

    Add project files.

---
+++ Assaignmnt03_1723744/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Assaignmnt03_1723744.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
